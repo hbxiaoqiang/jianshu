0b4b27e8fca277e8416c1a1c2699586062a5922f
hbxiaoqiang <hbxiaoqiang1@hotmail.com>
2020-01-10 23:48:15 +0800

    ma

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -6,9 +6,15 @@ class Header extends Component{
     render(){
         return(
             <HeaderWrapper>
-                <a href={'/'}>
+                <a className={'logo'} href={'/'}>
                     <img className={'img'} src={nav_log} alt=""/>
                 </a>
+
+                <a href={'/'} className={'login'}>登陆</a>
+                <ul className={'nav'}>
+                    <li className={'sy'}><a href={'/'}>首页</a></li>
+                    <li className={'xz'}><a href={'/'}>下载App</a></li>
+                </ul>
             </HeaderWrapper>
         )
     }
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,15 +1,38 @@
 import styled from 'styled-components';
 
+let wrapperHeight='56px';
+
 export const HeaderWrapper = styled.div`
     border-top:1px solid #ccc;
     border-bottom:1px solid #ccc;
-    a{
-        height:56px;
+    a.logo{
+        height:${wrapperHeight};
         width:100px;
         display:block;
         float:left;
     }
-    a .img{
+    a.logo .img{
         width:100%;
+    }
+
+    .nav{
+        margin:0 auto;
+        height:${wrapperHeight};
+        max-width:1024px;
+    }
+
+    .nav li{
+        float:left;
+        line-height:${wrapperHeight};
+        padding:0 20px
+    }
+
+    a.login{
+        height:${wrapperHeight};
+        line-height:${wrapperHeight};
+        width:100px;
+        display:block;
+        float:right;
+        
     }
 `
